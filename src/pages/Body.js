6364729818677466726494cf9f8aaedf1c039377
Nikhil Rawal <59576734+nikhil-rawal@users.noreply.github.com>
2024-12-22 20:00:04 -0500

    old body

--- src/pages/Body.js
+++ src/pages/Body.js
@@ -2,38 +2,34 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Main from "./Main";
 
-// const Body = () => {
-//   return (
-//     <div className="mx-2 px-4 my-4 py-4 w-full flex flex-col items-center">
-//       <div className="bg-gradient-to-r from-pink-500 to-violet-500 dark:from-blue-500 dark:to-green-500 h-2 w-9/12 blur-2xl"></div>
-//       <Header />
-//       <Main />
-//       <Footer />
-//       <div className="bg-gradient-to-l from-pink-500 to-violet-500 dark:from-blue-500 dark:to-green-500 h-2 w-9/12 blur-2xl"></div>
-//     </div>
-//   );
-// };
-
-// export default Body;
-
 const Body = () => {
   return (
-    <div className="min-h-screen flex flex-col relative">
-      <div className="bg-gradient-to-r from-pink-500 to-violet-500 dark:from-blue-500 dark:to-green-500 h-2 w-9/12 blur-2xl mx-auto"></div>
-
+    <div className="mx-2 px-4 my-4 py-4 w-full flex flex-col items-center">
+      <div className="bg-gradient-to-r from-pink-500 to-violet-500 dark:from-blue-500 dark:to-green-500 h-2 w-9/12 blur-2xl"></div>
       <Header />
-
-      <main className="flex-grow z-10 w-full flex justify-center">
-        <Main />
-      </main>
-
-      <footer className="w-full z-20 relative">
-        <Footer />
-      </footer>
-
-      <div className="bg-gradient-to-l from-pink-500 to-violet-500 dark:from-blue-500 dark:to-green-500 h-2 w-9/12 blur-2xl mx-auto"></div>
+      <Main />
+      <Footer />
+      <div className="bg-gradient-to-l from-pink-500 to-violet-500 dark:from-blue-500 dark:to-green-500 h-2 w-9/12 blur-2xl"></div>
     </div>
   );
 };
 
 export default Body;
+
+// const Body = () => {
+//   return (
+//     <div className="min-h-screen flex flex-col relative">
+//       <div className="bg-gradient-to-r from-pink-500 to-violet-500 dark:from-blue-500 dark:to-green-500 h-2 w-9/12 blur-2xl mx-auto"></div>
+//       <Header />
+//       <main className="flex-grow z-10 w-full flex justify-center">
+//         <Main />
+//       </main>
+//       <footer className="w-full z-20 relative">
+//         <Footer />
+//       </footer>
+//       <div className="bg-gradient-to-l from-pink-500 to-violet-500 dark:from-blue-500 dark:to-green-500 h-2 w-9/12 blur-2xl mx-auto"></div>
+//     </div>
+//   );
+// };
+
+// export default Body;
