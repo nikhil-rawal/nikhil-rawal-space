a72795a5104211b2511f9bc7e173bf1d317df5b2
Nikhil Rawal <59576734+nikhil-rawal@users.noreply.github.com>
2024-12-25 00:21:15 -0500

    working on skills section

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,6 +2,9 @@ import React from "react";
 import MainSummary from "../components/MainSummary";
 import MainProjects from "../components/MainProjects";
 import Footer from "./Footer";
+import Skills from "./Skills";
+import { FaLink } from "react-icons/fa";
+import { PiBracketsCurlyBold } from "react-icons/pi";
 
 const Main = () => {
   return (
@@ -14,12 +17,29 @@ const Main = () => {
         Toronto, Canada • UTC/GMT -5
       </div>
       <div className="flex flex-col py-10 h-1 ">
-        <h1 className="sm:text-4xl text-2xl justify-center mx-auto font-semibold">
+        <h1 className="sm:text-4xl text-2xl justify-center mx-auto font-semibold flex items-center">
+          <span className="mx-2 px-2">
+            <FaLink />
+          </span>
           My Projects
         </h1>
         <br />
         <MainProjects />
-        <div>
+        <br />
+        {/* Skills */}
+        <h1 className="sm:text-4xl text-2xl justify-center mx-auto font-semibold flex pt-2 mt-2 items-center">
+          <span className="mx-2 px-2">
+            <PiBracketsCurlyBold />
+          </span>
+          My Stacks
+        </h1>
+        <br />
+        <div className="skills-section py-2 my-2 ">
+          <Skills />
+        </div>
+        <br />
+        {/* Footer */}
+        <div className="footer-section py-2 my-2 ">
           <Footer />
           <div className="bg-gradient-to-l from-pink-500 to-violet-500 dark:from-blue-500 dark:to-green-500 h-2 w-9/12 blur-2xl"></div>
         </div>
---
+++ src/pages/Skills.js
@@ -0,0 +1,104 @@
+import React from "react";
+import { FaLinkedin } from "react-icons/fa";
+
+const Skills = () => {
+  return (
+    <div className="flex flex-row justify-center rounded-2xl shadow-lg bg-white dark:bg-neutral-900 p-6 py-8 h-full">
+      <div className=" hover:bg-stone-300 dark:hover:bg-stone-700 cursor-pointer rounded-xl p-2 mx-0 sm:mx-1 md:mx-2 lg:mx-3 flex justify-center content-center items-center">
+        <a
+          href="https://www.linkedin.com/in/nikhil-rawal/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="h-8 w-8 2xl:w-8 2xl:h-8 md:w-7 md:h-7 lg:h-7 lg:w-7 " />
+        </a>
+      </div>
+      <div className=" hover:bg-stone-300 dark:hover:bg-stone-700 cursor-pointer rounded-xl p-2 mx-0 sm:mx-1 md:mx-2 lg:mx-3 flex justify-center content-center items-center">
+        <a
+          href="https://www.linkedin.com/in/nikhil-rawal/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="h-8 w-8 2xl:w-8 2xl:h-8 md:w-7 md:h-7 lg:h-7 lg:w-7 " />
+        </a>
+      </div>
+      <div className=" hover:bg-stone-300 dark:hover:bg-stone-700 cursor-pointer rounded-xl p-2 mx-0 sm:mx-1 md:mx-2 lg:mx-3 flex justify-center content-center items-center">
+        <a
+          href="https://www.linkedin.com/in/nikhil-rawal/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="h-8 w-8 2xl:w-8 2xl:h-8 md:w-7 md:h-7 lg:h-7 lg:w-7 " />
+        </a>
+      </div>
+      <div className=" hover:bg-stone-300 dark:hover:bg-stone-700 cursor-pointer rounded-xl p-2 mx-0 sm:mx-1 md:mx-2 lg:mx-3 flex justify-center content-center items-center">
+        <a
+          href="https://www.linkedin.com/in/nikhil-rawal/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="h-8 w-8 2xl:w-8 2xl:h-8 md:w-7 md:h-7 lg:h-7 lg:w-7 " />
+        </a>
+      </div>
+      <div className=" hover:bg-stone-300 dark:hover:bg-stone-700 cursor-pointer rounded-xl p-2 mx-0 sm:mx-1 md:mx-2 lg:mx-3 flex justify-center content-center items-center">
+        <a
+          href="https://www.linkedin.com/in/nikhil-rawal/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="h-8 w-8 2xl:w-8 2xl:h-8 md:w-7 md:h-7 lg:h-7 lg:w-7 " />
+        </a>
+      </div>
+      <div className=" hover:bg-stone-300 dark:hover:bg-stone-700 cursor-pointer rounded-xl p-2 mx-0 sm:mx-1 md:mx-2 lg:mx-3 flex justify-center content-center items-center">
+        <a
+          href="https://www.linkedin.com/in/nikhil-rawal/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="h-8 w-8 2xl:w-8 2xl:h-8 md:w-7 md:h-7 lg:h-7 lg:w-7 " />
+        </a>
+      </div>
+      <div className=" hover:bg-stone-300 dark:hover:bg-stone-700 cursor-pointer rounded-xl p-2 mx-0 sm:mx-1 md:mx-2 lg:mx-3 flex justify-center content-center items-center">
+        <a
+          href="https://www.linkedin.com/in/nikhil-rawal/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="h-8 w-8 2xl:w-8 2xl:h-8 md:w-7 md:h-7 lg:h-7 lg:w-7 " />
+        </a>
+      </div>
+      <div className=" hover:bg-stone-300 dark:hover:bg-stone-700 cursor-pointer rounded-xl p-2 mx-0 sm:mx-1 md:mx-2 lg:mx-3 flex justify-center content-center items-center">
+        <a
+          href="https://www.linkedin.com/in/nikhil-rawal/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="h-8 w-8 2xl:w-8 2xl:h-8 md:w-7 md:h-7 lg:h-7 lg:w-7 " />
+        </a>
+      </div>
+      <div className=" hover:bg-stone-300 dark:hover:bg-stone-700 cursor-pointer rounded-xl p-2 mx-0 sm:mx-1 md:mx-2 lg:mx-3 flex justify-center content-center items-center">
+        <a
+          href="https://www.linkedin.com/in/nikhil-rawal/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="h-8 w-8 2xl:w-8 2xl:h-8 md:w-7 md:h-7 lg:h-7 lg:w-7 " />
+        </a>
+      </div>
+      <div className=" hover:bg-stone-300 dark:hover:bg-stone-700 cursor-pointer rounded-xl p-2 mx-0 sm:mx-1 md:mx-2 lg:mx-3 flex justify-center content-center items-center">
+        <a
+          href="https://www.linkedin.com/in/nikhil-rawal/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="h-9 w-9 2xl:w-8 2xl:h-8 md:w-7 md:h-7 lg:h-7 lg:w-7 " />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
+
+// html,css,javascript,typescript,react,nextjs,nodejs,redux,tailwind,figma,adobexd,sass/scss,git,github,mongodb,jest,vscode,mysql,aws,firebase,netlify,postman,graphql
+// heroku,vercel,photoshop,illustrator,premierepro,aftereffects,
